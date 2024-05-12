71f1883dc9a20556a73381f5c5912b168b73735e
Alireza Saffariyan <ar.saffariyan@gmail.com>
2024-05-12 21:54:10 +0330

    ⚡ DB and Docker configuration, and settings update
        📝 Modify '.gitignore' for better version control
        📝 Modify 'requirements.txt' for project dependencies
        🐳 Configure 'docker-compose.yml' for container orchestration
        🔧 Configure MySQL database in the project

--- mainProject/src/core/settings.py
+++ mainProject/src/core/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "projectDB",
+        "USER": "root",
+        "PASSWORD": "1234pass",
+        "HOST": "db",  # Or an IP Address that your DB is hosted on
+        "PORT": "3306",
     }
 }
 
